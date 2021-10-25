4d3337ca21139ac993be434e09b2c2a480824129
Daniel Ureña <67337592+dlurena24@users.noreply.github.com>
2021-10-24 21:56:11 -0600

    fixess

--- TestGenetico/TestGenetico.cpp
+++ TestGenetico/TestGenetico.cpp
@@ -22,7 +22,64 @@ uchar **best;
 uchar ***data, ***ndata;
 int *mpool;
 
-Species *ppl;
+struct Species
+{
+public:
+    int fit;
+    int _rw, _cl;
+    int index;
+
+    void setFit()
+    {
+        int cnt = 0;
+        for (int i = 0; i < _rw; i++)
+        {
+            for (int j = 0; j < _cl; j++)
+            {
+                if (data[index][i][j] == best[i][j])
+                    cnt++;
+            }
+        }
+        fit = 1000 * cnt / (_rw * _cl);
+        fit = (fit * fit) / 1000;
+    }
+
+    void allocate(int rw, int cl)
+    {
+        _rw = rw;
+        _cl = cl;
+        index = c_index;
+        for (int i = 0; i < _rw; i++)
+            for (int j = 0; j < _cl; j++)
+                data[c_index][i][j] = rand() % 256;
+        c_index++;
+    }
+
+    void copyImg(Mat m)
+    {
+        for (int i = 0; i < m.rows; i++)
+        {
+            uchar *lin = m.ptr(i);
+            for (int j = 0; j < m.cols; j++)
+            {
+                lin[j] = (uchar)data[index][i][j];
+            }
+        }
+    }
+
+    void mutate()
+    {
+        for (int i = 0; i < _rw; i++)
+        {
+            for (int j = 0; j < _cl; j++)
+            {
+                if (rand() % 100 < mrate)
+                    ndata[index][i][j] = rand() % 256;
+            }
+        }
+    }
+};
+
 
 int main(){srand(time(NULL));
 
@@ -33,6 +90,7 @@ int main(){srand(time(NULL));
     proc = target;
     frows = target.rows;
     fcols = target.cols;
+    Species *ppl;
 
     best = new uchar *[frows];
     for (int i = 0; i < frows; i++)
