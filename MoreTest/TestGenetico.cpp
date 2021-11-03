c44c688fc50e11aa28454479f464ae83ea941fe3
Daniel Ureña <67337592+dlurena24@users.noreply.github.com>
2021-11-03 01:59:33 -0600

    Avances

--- MoreTest/TestGenetico.cpp
+++ MoreTest/TestGenetico.cpp
@@ -6,6 +6,8 @@
 //#include algorithm
 
 using namespace cv;
+using namespace std;
+
 
 int frows, fcols;
 int ppn, plen;
@@ -38,14 +40,24 @@ public:
         fit = (fit * fit) / 1000;
     }
 
+//    Se modifica
     void allocate(int rw, int cl)
     {
         _rw = rw;
         _cl = cl;
         index = c_index;
         for (int i = 0; i < _rw; i++)
-            for (int j = 0; j < _cl; j++)
-                data[c_index][i][j] = rand() % 256;
+//            for (int j = 0; j < _cl; j++)
+//                data[c_index][i][j] = rand() % 256;
+            if(i > 50 && i < 75){
+                for (int j = 0; j < _cl; j++)
+                    data[c_index][i][j] = rand() % 256;
+            }
+            else{
+                for (int j = 0; j < _cl; j++) {
+                    data[c_index][i][j] = best[i][j];
+                }
+            }
         c_index++;
     }
 
@@ -65,11 +77,18 @@ public:
     {
         for (int i = 0; i < _rw; i++)
         {
-            for (int j = 0; j < _cl; j++)
-            {
-                if (rand() % 100 < mrate)
-                    ndata[index][i][j] = rand() % 256;
+            if (i > 50 && i < 75){
+                for (int j = 0; j < _cl; j++)
+                {
+                    if (rand() % 100 < mrate)
+                        ndata[index][i][j] = rand() % 256;
+                }
             }
+//            for (int j = 0; j < _cl; j++)
+//            {
+//                if (rand() % 100 < mrate)
+//                    ndata[index][i][j] = rand() % 256;
+//            }
         }
     }
 };
@@ -107,19 +126,72 @@ bool sortRule(Species a, Species b)
 int main(){srand(time(NULL));
 
     Mat target, proc;
-    target = imread("1.jpeg", 0);
+    target = imread("zz.png", 0);
     namedWindow("Display Image", WINDOW_AUTOSIZE);
 
-    /*
-    imshow("Display Image", target);
-    waitKey(0);
-    */
+//    --------------------------------------------------------------
+//      Test
 
+//    Mat target, proc;
+//    target = imread("zz.png", 1);
+//
+//    rectangle(target, Point(50,25), Point(100, 75), Scalar(255,0,0));
+//
+//    namedWindow("Display Image", WINDOW_AUTOSIZE);
+//
+//    imshow("Display Image", target);
+//    waitKey(0);
+
+
+//      -------------       ----------------        --------------    ------------ Masks
+////  BGR
+////  Get red
+//    Mat OutImg, OutRed;
+//    inRange(target, Scalar(0, 0, 95), Scalar(150, 150, 255) , OutImg);
+////    bitwise_and(OutImg, target, OutRed);
+//
+////  Get blue
+//    Mat OutImg2;
+//    inRange(target, Scalar(100, 0, 0), Scalar(255, 210, 210) , OutImg2);
+//
+////  Get green
+//    Mat OutImg3;
+//    inRange(target, Scalar(0, 100, 0), Scalar(100, 255, 100) , OutImg3);
+//
+////  Get white
+//    Mat OutImg4;
+//    inRange(target, Scalar(240, 240, 240), Scalar(255, 255, 255) , OutImg4);
+//
+//    int x1, x2, x3;
+//    x1 = countNonZero(OutImg);
+//    x2 = countNonZero(OutImg2);
+//    x3 = countNonZero(OutImg3);
+//
+//    if(x1>x2+x3){
+//        cout << "Red" << endl;
+//    }
+//    if(x2>x1+x3){
+//        cout << "Blue" << endl;
+//    }
+//    if(x3>x1+x2){
+//        cout << "Green" << endl;
+//    }
+//
+//    cout << x1 << "  " << x2 << "  " << x3;
+//
+//    namedWindow("Output Image", WINDOW_AUTOSIZE);
+//    imshow("Output Image", OutImg3); //Cambia segun el color
+//    waitKey(0);
+
+//    --------------------------------------------------------------------- EndTest
 
     proc = target;
+
     frows = target.rows;
     fcols = target.cols;
     Species *ppl;
+
+    cout << fcols << "  x  " << frows << "  pixeles" << endl;
 
     best = new uchar *[frows];
     for (int i = 0; i < frows; i++)
@@ -133,8 +205,16 @@ int main(){srand(time(NULL));
         {
             best[i][j] = (uchar)lin[j];
         }
+//        if (i > 50 && i < 75){
+//            uchar *lin = target.ptr(i);
+//            for (int j = 0; j < fcols; j++)
+//            {
+//                best[i][j] = (uchar) lin[j];
+//            }
+//        }
     }
-    std::cin >> ppn;
+//    std::cin >> ppn;
+    ppn = 75;
     mpool = new int[ppn * (ppn + 1) / 2];
 
     data = new uchar **[ppn + 10];
