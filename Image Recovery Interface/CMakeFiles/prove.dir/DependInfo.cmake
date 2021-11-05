725baea3f9b8e8bbc2dcdbdc260db206147550a9
Daniel Ureña <67337592+dlurena24@users.noreply.github.com>
2021-11-04 20:01:54 -0600

    cmake prove done

---
+++ Image Recovery Interface/CMakeFiles/prove.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/dlurena24/Documents/TEC_DOCS/ALGORITMOS_Y_ESTRUCTURAS_DE_DATOS_II/Projecto_2/Proyecto2_IR_RC/Image Recovery Interface/main.cpp" "/home/dlurena24/Documents/TEC_DOCS/ALGORITMOS_Y_ESTRUCTURAS_DE_DATOS_II/Projecto_2/Proyecto2_IR_RC/Image Recovery Interface/CMakeFiles/prove.dir/main.cpp.o"
+  "/home/dlurena24/Documents/TEC_DOCS/ALGORITMOS_Y_ESTRUCTURAS_DE_DATOS_II/Projecto_2/Proyecto2_IR_RC/Image Recovery Interface/mainwindow.cpp" "/home/dlurena24/Documents/TEC_DOCS/ALGORITMOS_Y_ESTRUCTURAS_DE_DATOS_II/Projecto_2/Proyecto2_IR_RC/Image Recovery Interface/CMakeFiles/prove.dir/mainwindow.cpp.o"
+  "/home/dlurena24/Documents/TEC_DOCS/ALGORITMOS_Y_ESTRUCTURAS_DE_DATOS_II/Projecto_2/Proyecto2_IR_RC/Image Recovery Interface/prove_autogen/mocs_compilation.cpp" "/home/dlurena24/Documents/TEC_DOCS/ALGORITMOS_Y_ESTRUCTURAS_DE_DATOS_II/Projecto_2/Proyecto2_IR_RC/Image Recovery Interface/CMakeFiles/prove.dir/prove_autogen/mocs_compilation.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "prove_autogen/include"
+  "/home/dlurena24/Programs/Qt/6.0.4/gcc_64/include/QtWidgets"
+  "/home/dlurena24/Programs/Qt/6.0.4/gcc_64/include"
+  "/home/dlurena24/Programs/Qt/6.0.4/gcc_64/include/QtCore"
+  "/home/dlurena24/Programs/Qt/6.0.4/gcc_64/mkspecs/linux-g++"
+  "/home/dlurena24/Programs/Qt/6.0.4/gcc_64/include/QtGui"
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
